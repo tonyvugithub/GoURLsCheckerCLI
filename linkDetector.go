5b321c8003f02397c99636ced970a7ea466886c3
tonyvugithub <55034244+tonyvugithub@users.noreply.github.com>
2020-09-21 12:24:16 -0400

    added version flag and check sub command

--- linkDetector.go
+++ linkDetector.go
@@ -109,36 +109,57 @@ func parseLinks(data string) []string {
 }
 
 func main() {
-
 	var upLinks []string
 	var downLinks []string
 
 	channel := make(chan linkStatus)
 
+	flagVersion := flag.Bool("version", false, "version")
+	checkCmd := flag.NewFlagSet("check", flag.ExitOnError)
 	//Parse command-line args
 	flag.Parse()
-	args := flag.Args()
 
-	checkValidArgsLen(args)
-
-	links := parseLinks(readFromFile(args[0]))
-
-	for _, link := range links {
-		go checkLink(link, channel)
+	if *flagVersion {
+		fmt.Println("Print version")
+		return
 	}
 
-	i := 0
-	for i < len(links) {
-		ls := <-channel
-		if ls.live == false {
-			downLinks = append(downLinks, ls.url)
-		} else {
-			upLinks = append(upLinks, ls.url)
+	if len(os.Args) < 2 {
+		displayHelpPanel()
+		os.Exit(0)
+	}
+
+	switch os.Args[1] {
+	case "check":
+		flags := os.Args[2:]
+
+		checkCmd.Parse(flags)
+		args := checkCmd.Args()
+		checkValidArgsLen(args)
+
+		links := parseLinks(readFromFile(args[0]))
+
+		for _, link := range links {
+			go checkLink(link, channel)
 		}
-		i++
-	}
 
-	fmt.Println("Total links:", len(links))
-	fmt.Println("Up links:", len(upLinks))
-	fmt.Println("Down links:", len(downLinks))
+		i := 0
+		for i < len(links) {
+			ls := <-channel
+			if ls.live == false {
+				downLinks = append(downLinks, ls.url)
+			} else {
+				upLinks = append(upLinks, ls.url)
+			}
+			i++
+		}
+
+		fmt.Println("Total links:", len(links))
+		fmt.Println("Up links:", len(upLinks))
+		fmt.Println("Down links:", len(downLinks))
+	default:
+		fmt.Println("Expected 'check' command")
+		fmt.Println("Eg: $ linkDetector check ...")
+		os.Exit(1)
+	}
 }
