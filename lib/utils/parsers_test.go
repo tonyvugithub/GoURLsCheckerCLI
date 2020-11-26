4ec4dfe4a798893af208af77a35e9aa945437e8a
tonyvugithub <55034244+tonyvugithub@users.noreply.github.com>
2020-11-26 13:26:38 -0500

    Added more test to Parsers packages

--- lib/utils/parsers_test.go
+++ lib/utils/parsers_test.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"os"
+	"os/exec"
 	"testing"
 )
 
@@ -26,4 +28,32 @@ func TestParseLinksWithInvalidURL(t *testing.T) {
 	if len(result) != 0 {
 		t.Errorf("ParseLink was incorrect, got: %d, want: %d.", len(result), 0)
 	}
+}
+
+func TestParseIgnoreListPatternWithValidLink(t *testing.T) {
+	data := "https://www.example.com/main.html"
+
+	result := ParseIgnoreListPattern(data)
+
+	if len(result) == 0 {
+		t.Errorf("ParseIgnoreListPattern was incorrect, got %d, want: %d", len(result), 1)
+	}
+}
+
+func TestParseIgnoreListPatternWithInvalidLink(t *testing.T) {
+	data := "www.example.com/main.html"
+
+	// Mocking an os.Exit(1) scenario
+	if os.Getenv("BE_CRASHER") == "1" {
+		ParseIgnoreListPattern(data)
+		return
+	}
+
+	cmd := exec.Command(os.Args[0], "-test.run=TestParseIgnoreListPatternWithInvalidLink")
+	cmd.Env = append(os.Environ(), "BE_CRASHER=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return
+	}
+	t.Fatalf("process ran with err %v, want exit status 1", err)
 }
